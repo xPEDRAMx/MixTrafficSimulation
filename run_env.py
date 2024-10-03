ce1e27e385ee9a7a50338dbf2dbb1662c3fd57b5
Pedram <123489525+xPEDRAMx@users.noreply.github.com>
2024-10-03 11:07:31 -0400

    Add files via upload

---
+++ run_env.py
@@ -0,0 +1,64 @@
+
+import logging
+logging.getLogger('gymnasium').setLevel(logging.ERROR)
+import pandas as pd
+import gymnasium as gym
+import sys
+sys.path.insert(0, "C:\\Users\\Pedram\\anaconda3\\Lib\\site-packages\\")
+import highway_env
+from highway_env import vehicle
+from highway_env import utils
+
+# Initialize the environment with 'human' render mode
+env = gym.make('midblock', render_mode='rgb_array',config={'action': {'type': 'NoAction'}})
+
+#env.unwrapped.toggle_traffic_signal(False)
+env.unwrapped.config["enable_pedestrians"] = False
+env.unwrapped.config["lanes_count"] = 3
+env.unwrapped.config["max_vehicles"] = 1
+env.unwrapped.config["duration"] = 5
+env.unwrapped.config["generation_interval"] = 3.0
+env.unwrapped.config["other_vehicles_type"] = "highway_env.vehicle.behavior.IDMVehicle"
+
+#TimeToCollisionObservation
+#env.unwrapped.config["other_vehicles_type"] = "highway_env.vehicle.behavior.IDMVehicle"
+env.reset()
+
+for lane_index, lane in enumerate(env.unwrapped.road.network.lanes_list()):
+    if lane_index == 0:
+        lane.speed_limit = 20.0  # Set speed limit for lane 0 (left lane)
+    elif lane_index == 1:
+        lane.speed_limit = 20.0  # Set speed limit for lane 1 (middle lane)
+    elif lane_index == 2:
+        lane.speed_limit = 20.0  # Set speed limit for lane 2 (right lane)
+
+# Set target speed for controlled vehicles
+"""
+if env.unwrapped.road.vehicles:
+    for controlled_vehicle in env.unwrapped.road.vehicles[:env.unwrapped.config["controlled_vehicles"]]:
+        controlled_vehicle.target_speed = 20.0  # Set the new target speed in m/s
+"""
+
+# Run the simulation loop
+for _ in range(100):
+    # Take an action (IDLE in this case)
+    #action = env.unwrapped.action_type.actions_indexes["IDLE"]
+    obs, reward, done, truncated, info = env.step("NoAction")
+    
+    # Render the environment (this will open a window and display the frames)
+    env.render()
+    
+    # Exit if the environment signals that it is done
+    if done:
+        break
+
+# Close the environment properly
+env.close()
+if hasattr(env, 'unwrapped'):
+    env = env.unwrapped  # This unwraps the environment to access the base class
+
+env.save_vehicle_info_csv("C:/Users/Pedram/Desktop/test_vehicle_info.csv")
+env.save_trajectories_csv("C:/Users/Pedram/Desktop/test_trajectories.csv")
+
+df = pd.read_csv("C:/Users/Pedram/Desktop/test_trajectories.csv")  # Update the path if necessary
+utils.plot_time_x_for_each_lane_and_id(df)
